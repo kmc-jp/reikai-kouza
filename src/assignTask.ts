8676768dbc630b31884b4c4da480ec30f0231c22
ryokohbato <ryokohbato@gmail.com>
2021-11-18 02:30:28 +0900

    ✨ 担当者にメッセージを送信
    日付周りのフォーマットも

--- src/assignTask.ts
+++ src/assignTask.ts
@@ -1,5 +1,6 @@
 import { assign } from "./assign";
 import { projectConstants } from "./modules/constants";
+import { toDate } from "./modules/date";
 import { executeQuery } from "./modules/mysql";
 import { postText } from "./modules/slack";
 
@@ -7,10 +8,7 @@ const argv = require("minimist")(process.argv.slice(2));
 
 const assignTask = async () => {
   const today_str = (argv["_"][0] as number).toString();
-  const today = new Date();
-  today.setFullYear(Number.parseInt(today_str.substring(0, 4)));
-  today.setMonth(Number.parseInt(today_str.substring(4, 6)) - 1);
-  today.setDate(Number.parseInt(today_str.substring(6, 8)));
+  const today = toDate(today_str);
 
   // 今日が月曜日か木曜日でなかった場合は終了
   if (today.getDay() != 1 && today.getDay() != 4) {
