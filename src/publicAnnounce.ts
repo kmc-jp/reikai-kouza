c7e9feb1f8f8990dad8cddce7fa8cf7f89f540bd
ryokohbato <ryokohbato@gmail.com>
2021-11-20 22:05:38 +0900

    🎨

--- src/publicAnnounce.ts
+++ src/publicAnnounce.ts
@@ -11,17 +11,17 @@ const publicAnnounce = async () => {
   const today_oneWeekAfter = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
   const today_oneWeekAfter__dbFormat = toDBFormat(today_oneWeekAfter);
 
-  const assignmentInfo = await executeQuery<tableStructure__ID & tableStructure__assignedDate>(`SELECT ${tableItemName.id}, ${tableItemName.assignedDate} FROM ${projectConstants.mysql.tableName} WHERE (${tableItemName.assignedDate} >= ? AND ${tableItemName.assignedDate} <= ?)`,
-  [
-    today__dbFormat,
-    today_oneWeekAfter__dbFormat,
-  ]);
+  const assignmentInfo = await executeQuery<tableStructure__ID & tableStructure__assignedDate>(
+    `SELECT ${tableItemName.id}, ${tableItemName.assignedDate} FROM ${projectConstants.mysql.tableName} WHERE (${tableItemName.assignedDate} >= ? AND ${tableItemName.assignedDate} <= ?)`,
+    [today__dbFormat, today_oneWeekAfter__dbFormat]
+  );
 
-  const assignmentInfoText = assignmentInfo.map(x => {
+  const assignmentInfoText = assignmentInfo.map((x) => {
     return `${format(toDate(x.assigned_date))}: <@${x.id}>`;
-  })
+  });
 
+  await postText("直近1週間の講座担当者を告知します。");
   await postText2Members(`直近1週間の講座担当者\n${assignmentInfoText.join("\n")}`);
-}
+};
 
 publicAnnounce();
