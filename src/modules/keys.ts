194d9e4db6c473974e96e886a510c0ae0c9dbc5b
ryokohbato <ryokohbato@gmail.com>
2022-03-06 12:23:52 +0900

    fix #138

--- src/modules/keys.ts
+++ src/modules/keys.ts
@@ -5,7 +5,7 @@ import type { Keys } from "../@types/keys";
 const path = require("path");
 
 // keys.jsonの内容を取得
-export const getKeys = async (isNotTest: boolean): Promise<Keys> => {
+export const getKeys = async (isNotTest: boolean = true): Promise<Keys> => {
   const keyReader = isNotTest
     ? readFile(path.join(__dirname, "./secret/keys.json"), "utf-8")
     : readFile(path.join(__dirname, "../../dist/secret/keys.json"), "utf-8");
