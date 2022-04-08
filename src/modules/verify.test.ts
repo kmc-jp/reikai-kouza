4ba40f4daacf06c9b271b92cdb5ff82f4acdab33
ryokohbato <ryokohbato@gmail.com>
2022-04-09 02:57:29 +0900

    テスト名を分かりやすく

--- src/modules/verify.test.ts
+++ src/modules/verify.test.ts
@@ -1,11 +1,11 @@
 import { verify } from "./verify";
 
-test("this is test", async () => {
+test("TEST: Signature Verification", async () => {
   expect(
     await verify(
       "v0=d3a8bc60a23c9d9837d70fbbd7694f62435f5618d084f5cc5af6d776e49b777f",
       "1645650000.100000",
-      "sample payload",
+      "sample payload"
     )
   ).toBe(true);
 });
