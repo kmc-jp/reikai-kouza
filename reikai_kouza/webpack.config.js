ae5521e617f2b3db828506a016148bbf082d99b6
ryokohbato <ryokohbato@gmail.com>
2022-05-31 23:42:32 +0900

    ビルドコマンドをディレクトリの変更に追従

--- reikai_kouza/webpack.config.js
+++ reikai_kouza/webpack.config.js
@@ -5,18 +5,18 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: {
-    "additionalAssignTask": "./src/additionalAssignTask.ts",
-    "assignTask": "./src/assignTask.ts",
-    "background": "./src/background.ts",
-    "publicAnnounce": "./src/publicAnnounce.ts",
-    "registerExistingMembers": "./src/registerExistingMembers.ts",
-    "send2AllMembers": "./src/send2AllMembers.ts",
-    "updateMembers": "./src/updateMembers.ts",
-    "updateStatus": "./src/updateStatus.ts",
+    "additionalAssignTask": "./reikai_kouza/src/additionalAssignTask.ts",
+    "assignTask": "./reikai_kouza/src/assignTask.ts",
+    "publicAnnounce": "./reikai_kouza/src/publicAnnounce.ts",
+    "registerExistingMembers": "./reikai_kouza/src/registerExistingMembers.ts",
+    "reikai_kouza": "./reikai_kouza/src/reikai_kouza.ts",
+    "send2AllMembers": "./reikai_kouza/src/send2AllMembers.ts",
+    "updateMembers": "./reikai_kouza/src/updateMembers.ts",
+    "updateStatus": "./reikai_kouza/src/updateStatus.ts",
   },
   output: {
     filename: "[name].js",
-    path: path.join(__dirname, "dist/"),
+    path: path.join(__dirname, "../dist/reikai_kouza"),
   },
   target: "node",
   module: {
